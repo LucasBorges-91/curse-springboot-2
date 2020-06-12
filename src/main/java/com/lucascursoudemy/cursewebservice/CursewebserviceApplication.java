2da7f443d81452cb2f6462972152cbf40a841901
_lucasborges <lucas.pborges@yahoo.com.br>
2020-06-12 18:34:33 -0300

    Project created

---
+++ src/main/java/com/lucascursoudemy/cursewebservice/CursewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lucascursoudemy.cursewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursewebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucascursoudemy/cursewebservice/CursewebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascursoudemy.cursewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursewebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
